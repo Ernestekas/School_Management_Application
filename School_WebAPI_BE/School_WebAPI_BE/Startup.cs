edd4706e971fb9602587cfd24ef05dee458c3e66
Ernestekas <ernestekas@gmail.com>
2022-02-09 19:15:47 +0200

    BE. Work Environment setup finished.

--- School_WebAPI_BE/School_WebAPI_BE/Startup.cs
+++ School_WebAPI_BE/School_WebAPI_BE/Startup.cs
@@ -2,11 +2,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using School_WebAPI_BE.Data;
+using School_WebAPI_BE.Validation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,6 +31,12 @@ namespace School_WebAPI_BE
         {
 
             services.AddControllers();
+
+            var defaultConnection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<DataContext>(d => d.UseSqlServer(defaultConnection));
+
+            services.AddAutoMapper(typeof(MappingProfiles));
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "School_WebAPI_BE", Version = "v1" });
