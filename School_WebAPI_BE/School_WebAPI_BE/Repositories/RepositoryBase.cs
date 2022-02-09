65ad98568378e9cc9dd0908e48bfcb4fd797ede0
Ernestekas <ernestekas@gmail.com>
2022-02-09 19:40:02 +0200

    RepositoryBase added.

---
+++ School_WebAPI_BE/School_WebAPI_BE/Repositories/RepositoryBase.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using School_WebAPI_BE.Data;
+using School_WebAPI_BE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School_WebAPI_BE.Repositories
+{
+    public abstract class RepositoryBase<T> where T : Entity
+    {
+        protected DataContext _context;
+        private DbSet<T> _dbSet;
+
+        protected RepositoryBase(DataContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int entityId)
+        {
+            return await _dbSet.FirstOrDefaultAsync(x => x.Id == entityId);
+        }
+
+        public async Task<int> CreateAsync(T entity)
+        {
+            _context.Add(entity);
+            await _context.SaveChangesAsync();
+            return entity.Id;
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoveAsync(T entity)
+        {
+            _context.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
