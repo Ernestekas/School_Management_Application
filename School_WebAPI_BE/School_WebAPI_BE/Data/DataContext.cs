acdab21ebd8715fd957f73bf759a3897085fdaba
Ernestekas <ernestekas@gmail.com>
2022-02-09 19:23:37 +0200

    BE. Student and School models added.

--- School_WebAPI_BE/School_WebAPI_BE/Data/DataContext.cs
+++ School_WebAPI_BE/School_WebAPI_BE/Data/DataContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using School_WebAPI_BE.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,8 @@ namespace School_WebAPI_BE.Data
 {
     public class DataContext : DbContext
     {
+        public DbSet<School> Schools { get; set; }
+        public DbSet<Student> Students { get; set; }
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
     }
 }
